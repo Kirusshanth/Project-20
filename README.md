c2fb902b8cfbef8f621fea630db73b638ae8f350
Kirusshanth <78604022+Kirusshanth@users.noreply.github.com>
2021-03-05 09:55:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project-20
-nice
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var car, wall
+var speed,weight
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(55,90)
+  weight=random(400,1500)
+  car=createSprite(400, 200, 50, 50);
+  car.velocityX = speed;
+
+  wall=createSprite(1500,200,60,height/2)
+}
+
+function draw() {
+  background(255,255,255); 
+
+    if(wall.x-car.x<(car.width+wall.width)/2)
+  {
+    if(hasCollided(bullet, wall))
+    {
+    car.velocity=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if(deformation>180)
+    {
+      car.shapecolor=color(255,0,0)
+    }
+    if(deformation<180 && deformation>100)
+    {
+      car.shapecolor=color(230,230,0)
+    }
+    if(deformation<100)
+    {
+      car.shapecolor=color(0,255,0)
+    }
+  }
+}
+
+  
+  
+  drawSprites();
+}
